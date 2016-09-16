d8632ac7e6bc8d4b0de9c5b80f28151616c62ca4
Johan Aschan <johan.aschan@svt.se>
2016-09-16 15:46:15 +0200

    ticketnumber

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -18,16 +18,16 @@ describe('App: QueueTicketWeb', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
+  it('should have as ticketNumber 2', async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     let app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
+    expect(app.ticketNumber).toEqual(2);
   }));
 
   it('should render title in a h1 tag', async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     let compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    expect(compiled.querySelector('h1').textContent).toContain('QueueTicket');
   }));
 });
