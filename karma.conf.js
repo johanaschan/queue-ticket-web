cee6111e26b7c8852e582da503649d1a1f98b678
Johan Aschan <johan.aschan@svt.se>
2016-09-19 15:59:19 +0200

    remove space

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/0.13/config/configuration-file.html
 
-
 module.exports = function (config) {
 
   const configuration = {
