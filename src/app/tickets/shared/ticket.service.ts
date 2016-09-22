8f8444d518eb509a9f973cbb35212559c15377ba
Johan Aschan <aschan.johan@gmail.com>
2016-09-22 23:03:20 +0200

    reset

--- src/app/tickets/shared/ticket.service.ts
+++ src/app/tickets/shared/ticket.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import '../../shared';
 import {Ticket} from './ticket';
-import {Http, Response, Headers, RequestOptions} from '@angular/http';
+import {Http, Response} from '@angular/http';
 import {Observable} from 'rxjs';
 
 @Injectable()
