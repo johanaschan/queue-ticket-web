b58a4aed42f9820daf0b78493febe7cd15197eff
Lars Mårdberg <larsmardberg@gmail.com>
2016-11-13 11:12:48 +0100

    Added testcases to main component

--- src/app/main/main.component.spec.ts
+++ src/app/main/main.component.spec.ts
@@ -1,26 +1,50 @@
-/*it('should call authService should be called with role user have', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+import { TestBed} from '@angular/core/testing';
+import { MainComponent } from './main.component';
+import { AuthService } from '../security';
+import { Component } from '@angular/core';
+
+// tslint:disable-next-line:component-selector-prefix
+@Component({selector: 'router-outlet', template: ''})
+class RouterOutletStubComponent {
+}
+
+
+class AuthServiceStub {
+
+  hasRole(role: string): boolean {
+    if (role === 'role') {
+      return true;
+    }
+      return false;
+  }
+
+}
+
+
+describe('MainComponent', () => {
+
+ beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        MainComponent,
+        RouterOutletStubComponent
+      ],
+      providers: [
+        {provide: AuthService, useClass: AuthServiceStub}
+      ]
+    });
+  });
+
+
+  it('should call authService should be called with role user have', () => {
+    const fixture = TestBed.createComponent(MainComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.hasRole('role')).toBe(true);
   });
 
   it('should call authService should be called with role user do not have', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(MainComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.hasRole('noRole')).toBe(false);
   });
-
-class AuthServiceStub {
-
-hasRole(role: string): boolean {
-if (role === 'role') {
-return true;
-}
-return false;
-}
-
-isLoggedIn(): boolean {
-return true;
-}
-}
-*/
+});
